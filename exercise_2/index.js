f1f8e06ab8dd3890deaf73ab878829eba74e03a6
Brande Gilbert <brandegilbert@gmail.com>
2021-09-08 16:15:55 -0400

    Update

--- exercise_2/index.js
+++ exercise_2/index.js
@@ -21,6 +21,14 @@ const cities = [
     {cityName: "Paris", continent: "Europe"},    
 ];
 
+const myMovies = [
+    {movieName: "Skyfall", poster: "https://www.jamesbondlifestyle.com/sites/default/files/ckeditor/images/news/120809-james-bond-daniel-craig-skyfall.jpg"},
+    {movieName: "Live Free or Die Hard", poster:"https://cdn.shopify.com/s/files/1/0057/3728/3618/products/b197904bb75a1c3c9f55046083a94fa4_e88d5bb0-e1b7-4209-9f71-fb579f6c4461.jpg?v=1573651510"},
+    {movieName: "Free Solo", poster:"https://m.media-amazon.com/images/I/71jKZtjsgOL._AC_SY679_.jpg"},
+    {movieName: "Black Panther", poster:"https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_.jpg"},
+    {movieName: "Inside Out", poster:"https://target.scene7.com/is/image/Target/GUEST_47c335e5-6055-453c-99c9-d7f8d56fd227?wid=488&hei=488&fmt=pjpeg"}
+];
+
 
 // Write your routes between the ***
 
@@ -41,6 +49,18 @@ app.get("/error", (req, res) => {
     res.statusCode = 400
     res.send("This is an invalid route")
 })
+
+app.get("/info", (req, res) => {
+    res.send("{name: Brande, city: Atlanta, state: GA}")
+})
+
+app.get("/movies", (req, res) => {
+    res.render("movies", {
+        locals: {
+            myMovies: myMovies
+        },
+    })
+});
 
 // ***
 
