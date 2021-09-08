11d8b49555ddd3e05669723c5cb635063a0c4b37
Brande Gilbert <brandegilbert@gmail.com>
2021-09-08 15:16:26 -0400

    Part of exercise 2 complete

--- exercise_2/index.js
+++ exercise_2/index.js
@@ -2,9 +2,45 @@ const PORT = 3001
 const express = require("express")
 const app = express()
 
+const es6Renderer = require("express-es6-template-engine");
+
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
+
+const cities = [
+    {cityName: "Barcelona", continent: "Europe"},
+    {cityName: "Amsterdam", continent: "Europe"},   
+    {cityName: "Punta Cana", continent: "North America"},    
+    {cityName: "Sydney", continent: "Australia"},    
+    {cityName: "Portugal", continent: "Europe"},    
+    {cityName: "Washington DC", continent: "North America"},    
+    {cityName: "Miami", continent: "North America"},    
+    {cityName: "Phuket", continent: "Asia"},    
+    {cityName: "Bahia", continent: "South America"},    
+    {cityName: "Paris", continent: "Europe"},    
+];
+
+
 // Write your routes between the ***
 
 // ***
+app.get("/", (req, res) => {
+    res.send("Node and express are just lovely")
+})
+
+app.get("/cities", (req, res) => {
+    res.render("home", {
+        locals: {
+         cities: cities
+        },
+    })
+});
+
+app.get("/error", (req, res) => {
+    res.statusCode = 400
+    res.send("This is an invalid route")
+})
 
 // ***
 
